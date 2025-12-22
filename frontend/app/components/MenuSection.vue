f7e69d64ea7789e073f464d696512e9f10a3de74
faralha <faralhaonly@gmail.com>
2025-12-22 20:36:42 +0700

    feat: docker deployment

--- frontend/app/components/MenuSection.vue
+++ frontend/app/components/MenuSection.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="space-y-3">
     <h2 v-if="title" class="text-2xl font-semibold">{{ title }}</h2>
-    <div class="border border-default rounded-[10px] overflow-hidden">
+    <div class="border border-default rounded-[10px] overflow-hidden md:grid md:grid-cols-3">
       <template v-for="(item, index) in items" :key="item.to || index">
-        <NuxtLink :to="item.to" class="block transition-colors hover:bg-gray-50 dark:hover:bg-gray-800/50">
+        <NuxtLink :to="item.to" class="border border-default block transition-colors hover:bg-gray-50 dark:hover:bg-gray-800/50">
           <UPageCard :icon="item.icon" :title="item.label" :description="item.description" variant="ghost" />
         </NuxtLink>
-        <USeparator v-if="index < items.length - 1" />
       </template>
     </div>
   </div>
