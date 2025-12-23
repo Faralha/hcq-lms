2e20721ae22dc69ff8929b6d8f896a3cfe23bc09
faralha <faralhaonly@gmail.com>
2025-12-24 00:39:00 +0700

    fix: backend api url standarization

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Set global prefix
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('v1');
 
   // Serve static files for rapor PDFs
   // PDFs are stored in backend/rapor folder
