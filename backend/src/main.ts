0b0419ecb6b54f16818e4e3833a3c3ba5af112b6
Faralha <faralhaonly@gmail.com>
2026-01-07 21:54:32 +0700

    fix: api url main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -71,7 +71,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`🚀 Application is running on: http://localhost:${port}/api/v1`);
+  console.log(`🚀 Application is running on: http://localhost:${port}/v1`);
 }
 
 void bootstrap();
