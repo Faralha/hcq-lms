d5ef7e6da7a1f4381217440026e8065ec9e8b3df
Faralha <faralhaonly@gmail.com>
2026-07-20 19:52:17 +0700

    feat: add demo folder and GitHub Pages deployment workflow

---
+++ demo/nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/image', 'motion-v/nuxt'],
+  css: ['~/assets/css/main.css'],
+
+  ssr: false,
+  colorMode: { preference: 'light' },
+
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/hcq-lms/',
+    head: {
+      title: 'HCQ LMS - Demo',
+      htmlAttrs: { lang: 'id' },
+    },
+  },
+
+  spaLoadingTemplate: 'spaLoading.html',
+
+  nitro: {
+    prerender: {
+      failOnError: false,
+    },
+  },
+
+  vite: {
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: { drop_console: true, drop_debugger: true },
+      },
+    },
+  },
+})
